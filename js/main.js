3de2427ed252bf2d90d09e5af9f4347df27ad444
Luke Vander Poel <airscout99@gmail.com>
2016-07-17 21:54:33 -0700

    finished basic requirements

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 // WATS1020 Dom Manipulation
 // Custom script goes here.
 //////////////////////////////////////////////////
-
+// Place all your Javascript code inside this "document ready" function so
+// it does not run until the DOM is ready for Javascript manipulation.
 $( document ).ready(function() {
     var userInfo = {
         firstName: 'Jane',
@@ -13,17 +14,33 @@ $( document ).ready(function() {
         greatest: 0,
         total: 0
     };
-    // Place all your Javascript code inside this "document ready" function so
-    // it does not run until the DOM is ready for Javascript manipulation.
 
-    // TODO: Create a function to listen for clicks on the "login" button.
-    //      1. When a user clicks the "login" button, hide the login
-    //          form elements on the page.
-    //      2. Fill the user's first and last name into `div.user-info`.
-    //      (NOTE: You do not have to perform any validation on the data as
-    //          a base requirement.)
-
-
+  // Create a function to listen for clicks on the "login" button.
+    $('.login-btn').on('click', function(event){
+      console.log(event);
+      var targetElement = event.target;
+      var container = targetElement.parentElement.parentElement;
+      //      1. When a user clicks the "login" button, hide the login
+      //          form elements on the page.
+      $(container).find('#login-form').each(function(index, el){
+        if ($(el).is(':visible')){
+          $(el).fadeOut();
+          targetElement = '#login-form';
+        }
+      })
+      //      2. Fill the user's first and last name into `div.user-info`.
+      $(container).find('.user-info').each(function(index, el){
+        var userFirstName = userInfo.firstName;
+        var userLastName = userInfo.lastName;
+        $('.first-name').text(userFirstName);
+        $('.last-name').text(userLastName);
+        console.log(userFirstName, userLastName);
+        if ($(el).is(':hidden')){
+          $(el).fadeIn();
+          targetElement = '.user-info';
+        };
+      })
+    })
 
 
     // Create a function to listen for clicks on all the "View Details"
@@ -40,7 +57,7 @@ $( document ).ready(function() {
           //      3. Toggle visibility of all the elements within that parent with the class `details`.
             if ($(el).is(':visible')){
                 $(el).fadeOut();
-                targetElement.innerText = 'View Details'
+                targetElement.innerText = 'View Details';
             }
             //      4. Change the text of the "view details" button to read "hide details" so the user
             //          understands they can hide the text again.
@@ -51,20 +68,34 @@ $( document ).ready(function() {
         });
     });
 
-    $('.btn.vote').on('click', function(event){
-      console.log(event);
-      var targetElement = event.target;
-      
-    })
     // Create a function that listens for clicks on the voting buttons and
     // looks at the `data-vote` attribute on each button to see what was voted for,
     // then determines the updated vote breakdown to adjust the progress bars.
-
     //      1. Set up an event listener on the buttons with the `vote` class.
-    //      2. When a button is clicked, look at the `data-vote` attribute to determine
-    //          what the user is voting for ("great" or "greatest").
-    //      3. Increment the counter for whichever vote talley is affected.
-    //      4. Determine the respective percentages (out of 100) for each progress bar.
-    //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
+    $('.vote').on('click', function(event){
+      console.log(event);
+      voteCounts.total++;
+      var targetElement = event.target;
+      //      2. When a button is clicked, look at the `data-vote` attribute to determine
+      //          what the user is voting for ("great" or "greatest").
 
+      if ($(targetElement).data('vote') === 'great'){
+      //      3. Increment the counter for whichever vote talley is affected.
+      voteCounts.great++;
+      }
+
+      else {
+      //      3. Increment the counter for whichever vote talley is affected.
+        voteCounts.greatest++;
+      }
+
+      //      4. Determine the respective percentages (out of 100) for each progress bar.
+      var greatPercent = (voteCounts.great/voteCounts.total*100);
+      var greatestPercent = (voteCounts.greatest/voteCounts.total*100);
+
+      //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
+      $('.great-progress').css('width', greatPercent + '%');
+      $('.greatest-progress').css('width', greatestPercent + '%');
+      console.log(voteCounts.great, voteCounts.greatest, voteCounts.total, greatPercent, greatestPercent);
+    })
 });
